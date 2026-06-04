3b04ad831e9df3fa12801224cf240edc340fbd7d
Ezebed <obedvillegas.02@gmail.com>
2026-06-04 18:46:36 -0400

    feat(refactor/migrate-to-astro-framework-v2): Initialize new Astro project with essential files, migrate existing files, and update package.json and astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://raiteo.app",
+  output: "static",
+  build: {
+    assets: "assets",
+  },
+  server: {
+    port: 4321,
+  },
+});
