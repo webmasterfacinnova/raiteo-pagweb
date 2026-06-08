384169ed569d9527e23a88da1975ccbac19c3ba8
Ezebed <obedvillegas.02@gmail.com>
2026-06-08 15:52:11 -0400

    feat: Update Footer component to use Lucide icons and add contact links

--- src/lib/appConfigs.ts
+++ src/lib/appConfigs.ts
@@ -8,6 +8,8 @@ export const BUSSINESS_HOURS = "Lunes a Viernes, 8:00 – 17:00";
 export const playStoreUrl =
   "https://play.google.com/store/apps/details?id=com.facinnova.raiteo";
 
+export const linbkedInUrl = "https://www.linkedin.com/company/facinnova/";
+
 const DELETE_DATA_SUBJECT = `Solicitud de eliminacion de datos - ${APP_NAME}`;
 const DELETE_DATA_BODY = [
   `Hola equipo de ${APP_NAME},`,
